f71be0bef27102d07c9c40119ffa3b4317cf5699
Muhammad Umair <181370124@gift.edu.pk>
2022-07-15 13:27:49 +0500

    updation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   let time = new Date().toLocaleTimeString();
+  const [ctime, setCtime] = useState(time);
+
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  };
+  setInterval(updateTime, 1000);
+
   return (
+    <div>
     <div className="App">
       <header className="App-header">
-        <h1>{time}</h1>
+        <h1>Digital Clock</h1>
+        <h1>{ctime}</h1>
       </header>
+    </div>
     </div>
   );
 }
